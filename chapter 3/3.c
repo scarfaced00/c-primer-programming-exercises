15e7331842ea5437e2a869c64e1ff08e98879761
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2020-12-23 16:10:54 +0900

    Create 3.c

---
+++ chapter 3/3.c
@@ -0,0 +1,10 @@
+/* chapter 3-3 */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\aStartled by the sudden, Sally shouted,\n\"By the Great Pumpkin, what was that!\"");
+    //'\a'=escape sequence for an alert
+    
+    return 0;
+}
