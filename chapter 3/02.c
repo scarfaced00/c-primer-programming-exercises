c996dae1d7f75de3d3a0611717c799e1e3f5de0a
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2020-12-23 16:04:53 +0900

    Create 02.c

---
+++ chapter 3/02.c
@@ -0,0 +1,12 @@
+/* chapter 3-2 */
+#include <stdio.h>
+
+int main(void)
+{
+    int ascii;
+    printf("Enter an ASCII code value: ");
+    scanf("%d",&ascii);
+    printf("The character for ASCII code you just entered: %c",ascii);
+    
+    return 0;
+}
