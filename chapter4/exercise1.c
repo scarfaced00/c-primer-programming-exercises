459cd3b3d7328e82510ca028f3ec6721187db39e
scarfaced00 <67523253+scarfaced00@users.noreply.github.com>
2021-01-04 09:26:22 +0900

    Create exercise1.c

---
+++ chapter4/exercise1.c
@@ -0,0 +1,12 @@
+/*Chapter 4-1*/
+#include <stdio.h>
+
+int main() {
+    char first[20];
+    char last[20];
+    printf("Enter your first and name name(eg. Jane Doe) ");
+    scanf("%s %s",first, last);
+    printf("%s, %s\n",last,first);
+    return 0;
+}
+  
